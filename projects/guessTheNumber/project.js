76457826f315b4efce6189dcccc6b54a05fe5d25
Jeremiah Tampol <tampoljeremiah@gmail.com>
2026-09-01 09:03:32 +0800

    Commit

--- projects/guessTheNumber/project.js
+++ projects/guessTheNumber/project.js
@@ -41,3 +41,67 @@ function diceroll(){
 }
 
 
+
+
+
+// restaurant order
+
+const burger = document.getElementById("burger");
+const fries = document.getElementById("fries");
+const mcCafe = document.getElementById("mccafe");
+const foodPicked = document.getElementById("foodPicked");
+const price = document.getElementById("price");
+
+const decrease = document.getElementById("decreaseBtn");
+const increase = document.getElementById("increaseBtn");
+const reset = document.getElementById("resetBtn");
+const quantity = document.getElementById("quantity")
+
+let order = 1;
+let selectedPrice = 0;
+
+const burgerPrice = 50;
+const friesPrice = 50;
+const mcCafePrice = 100;
+
+function updateOrder() {
+    quantity.textContent = order;
+    price.textContent = `$${selectedPrice * order}`;
+}
+
+function chooseFood(image, itemPrice) {
+    foodPicked.innerHTML = `<img src="${image}">`;
+    selectedPrice = itemPrice;
+    order = 1;
+    updateOrder();
+}
+
+burger.onclick = function () {
+    chooseFood("images/Burger.jpg", burgerPrice);
+};
+
+fries.onclick = function () {
+    chooseFood("images/Fries.jpg", friesPrice);
+};
+
+mcCafe.onclick = function () {
+    chooseFood("images/mcCafe.jpg", mcCafePrice);
+};
+
+increase.onclick = function () {
+    order++;
+    updateOrder();
+};
+
+decrease.onclick = function () {
+    if (order > 1) {
+        order--;
+        updateOrder();
+    }
+};
+
+reset.onclick = function () {
+    order = 1;
+    updateOrder();
+};
+
